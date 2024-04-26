a1c71c9ed753dda6b3b907afce1dcd319ae4b92f
Rek Dev <rekdev@keemail.me>
2024-04-25 22:07:11 -0600

    Add system theme preference detection and fix some problems in analysisArea overflow

--- src/components/AnalysisArea.jsx
+++ src/components/AnalysisArea.jsx
@@ -5,11 +5,11 @@ function AnalysisArea({ onChange, value }) {
 		<div className='grid h-auto grid-cols-1 font-noto-sans text-esgrammar-dark-100 *:border-esgrammar-light-400 *:bg-esgrammar-light-100 *:p-6 *:text-lg *:outline-none placeholder:*:text-esgrammar-light-700 md:h-60 md:grid-cols-2 dark:text-esgrammar-light-100 dark:*:border-esgrammar-dark-400 dark:*:bg-esgrammar-dark-300 dark:placeholder:*:text-esgrammar-dark-700'>
 			<textarea
 				placeholder='Ingrese o pegue algun texto'
-				className='min-h-40 resize-none rounded-t-lg border-x-2 border-y-2 border-esgrammar-light-400 md:rounded-l-lg md:rounded-r-none'
+				className='h-40 md:h-full resize-none rounded-t-lg border-x-2 border-y-2 border-esgrammar-light-400 md:rounded-l-lg md:rounded-r-none'
 				onChange={onChange}
 			/>
 			<div
-				className='min-h-40 overflow-auto rounded-b-lg border-x-2 border-b-2 border-esgrammar-light-400 md:rounded-l-none md:rounded-r-lg md:border-y-2 md:border-l-0'
+				className='h-40 md:h-full overflow-auto rounded-b-lg border-x-2 border-b-2 border-esgrammar-light-400 md:rounded-l-none md:rounded-r-lg md:border-y-2 md:border-l-0'
 				dangerouslySetInnerHTML={{ __html: value }}
 			/>
 		</div>
--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,18 +3,31 @@ import { useEffect, useState } from 'react'
 function useTheme() {
 	const [theme, setTheme] = useState(null)
 
+	const getSystemPreference = () =>
+		window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+
 	const toggleTheme = () => {
 		setTheme(theme === 'dark' ? 'light' : 'dark')
 	}
 
-	useEffect(() => setTheme(localStorage.getItem('theme')), [])
+	useEffect(() => {
+		setTheme(localStorage.getItem('theme'))
+
+		if (
+			localStorage.getItem('theme') === null &&
+			getSystemPreference() === 'dark'
+		)
+			setTheme('dark')
+	}, [])
 
 	useEffect(() => {
 		if (theme === 'dark') {
 			document.documentElement.classList.add('dark')
 		} else document.documentElement.classList.remove('dark')
 
-		if (theme !== null) localStorage.setItem('theme', theme)
+		if (theme !== null && theme !== getSystemPreference()) {
+			localStorage.setItem('theme', theme)
+		} else if (theme === getSystemPreference()) localStorage.removeItem('theme')
 	}, [theme])
 
 	return { theme, toggleTheme }
